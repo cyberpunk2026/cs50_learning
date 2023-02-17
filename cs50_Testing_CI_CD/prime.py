093fde7baf9fe5200f45166117368727f1d44e64
cyberpunk2026 <sydvill3@yahoo.com>
2023-02-17 23:45:58 +0200

    use wrong valid flight function

---
+++ cs50_Testing_CI_CD/prime.py
@@ -0,0 +1,12 @@
+# import math
+
+# def is_prime(n):
+#     if n < 2:
+#         return False
+#     for i in range(2, int(math.sqrt(n))):
+#         if n %  2 == 0:
+#             return False
+#     return True
+
+
+# print(is_prime(10))
